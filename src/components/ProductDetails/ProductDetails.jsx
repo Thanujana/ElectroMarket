b7b6eeadcf2a92db362e125754fa8ea6911c6b0b
Thanujana <janat-ec20030@stu.kln.ac.lk>
2024-12-16 23:20:50 +0530

    add some css file for productdetails page

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import './ProductDetails.css';
 import { varieties_list } from "../../assets/assets";
 
 const ProductDetails = () => {
